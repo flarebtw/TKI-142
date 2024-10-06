7938e04044208c4abef2a93c84dc2d5cf3410e15
flarebtw <egorskrip07@gmail.com>
2024-10-07 00:09:20 +0300

    Добавил задание 1.2

---
+++ Task1-2.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+/**
+* @brief Переводит значения массы из килограмм в унции
+* @param x значение массы в кг
+* @return возвращает рассчитаное значение
+*/
+double result(double x);
+
+/**
+* @brief точка входа в программу
+* @return 0 в случае успеха
+*/
+int main(void)
+{
+	double x;
+	puts("Введите вес в килограммах");
+	scanf_s("%lf", &x);
+	printf("%lf", result(x));
+	return 0;
+}
+
+double result(double x)
+{
+	const double ounce = 28.3;
+	return x / ounce;
+}
