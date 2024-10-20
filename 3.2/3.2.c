8e77bad09466df1bd63eee93eda238c4b7af5321
Egor Skripnikov <112763591+flarebtw@users.noreply.github.com>
2024-10-20 16:43:43 +0300

    Create 3.2.c

---
+++ 3.2/3.2.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <math.h>
+
+double input(void);
+
+double inputInt(void);
+
+double next_element(double previous_element, int k);
+
+double getNSum(int n);
+
+double getSumNotLessThanE(int n, const double e);
+
+int main(void)
+{
+	puts("Enter number of sequence elements:");
+	const int n = inputInt();
+	puts("Enter e value:");
+	const double e = input();
+	printf("")
+}
